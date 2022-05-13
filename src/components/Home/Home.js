53e5e3a8921674c13e961b61110f695f19cfb7d0
U Sen Wan <senwan96@gmail.com>
2022-05-13 15:30:05 +0600

    Delete a user from the database

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
 
     const handleUserDelete = id => {
-        
+        const proceed = window.confirm('Are you sure want to delete?')
+        if(proceed){
+            console.log('deleting user with id, ', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0){
+                    console.log('deleted');
+                    const remaining = users.filter(user => user._id !== id);
+                    setUsers(remaining);
+                }
+            })
+        }
     }
     useEffect(() => {
         fetch('http://localhost:5000/user')
@@ -16,6 +32,7 @@ const Home = () => {
             <h2>Available users: {users.length}</h2>
             <ul>
                 {users.map(user => <li key={user._id}>{user.name}: {user.email}
+                <Link to={`/update/${user._id}`}><button>Update</button></Link>
                 <button onClick={() => handleUserDelete(user._id)}>X</button>
                 </li>)}
             </ul>
